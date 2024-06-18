dd3ba1a18d05b68b60017c0456bf4d196fc627d2
ydshieh <ydshieh@users.noreply.github.com>
2024-06-18 09:30:01 +0200

    try soxr==0.4.0b1

--- .circleci/create_circleci_config.py
+++ .circleci/create_circleci_config.py
@@ -107,6 +107,10 @@ class CircleCIJob:
             "checkout",
             {"attach_workspace": {"at": "test_preparation"}},
         ]
+
+        # hack
+        self.install_steps.append("pip install soxr==0.4.0b1")
+
         steps.extend([{"run": l} for l in self.install_steps])
         steps.append({"run": {"name": "Show installed libraries and their size", "command": """du -h -d 1 "$(pip -V | cut -d ' ' -f 4 | sed 's/pip//g')" | grep -vE "dist-info|_distutils_hack|__pycache__" | sort -h | tee installed.txt || true"""}})
         steps.append({"run": {"name": "Show installed libraries and their versions", "command": """pip list --format=freeze | tee installed.txt || true"""}})
